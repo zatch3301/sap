7090da646ab6d9dddbf242c4f27d3f1ead9fe607
Gagan Verma <46402550+zatch3301@users.noreply.github.com>
2023-07-17 22:50:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,18 @@
 
   //I'm adding this section so I don't have to keep updating this pen every year :-)
   //remove this if you don't need it
-  // let today = new Date(),
-  //     dd = String(today.getDate()).padStart(2, "0"),
-  //     mm = String(today.getMonth() + 1).padStart(2, "0"),
-  //     yyyy = today.getFullYear(),
-  //     nextYear = yyyy + 1,
-  //     dayMonth = "02/14/",
-  //     birthday = dayMonth + yyyy;
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "02/14/",
+      birthday = dayMonth + yyyy;
   
-  // today = mm + "/" + dd + "/" + yyyy;
-  // if (today < birthday) {
-  //   birthday = dayMonth + nextYear;
-  // }
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today < birthday) {
+    birthday = dayMonth + nextYear;
+  }
   //end
   
   const countDown = new Date(birthday).getTime(),
@@ -32,14 +32,14 @@
           document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
         //do something later when date is reached
-        if (distance < 0) {
-          document.getElementById("headline").innerText = "Bonjour Chérie!";
-          document.getElementById("countdown").style.display = "none";
-          document.getElementById("canvas").style.display = "none";
-          document.body.style.backgroundColor = "#ffd54f"; 
-          document.getElementById("content").style.display = "block";
-          clearInterval(x);
-        }
+        // if (distance < 0) {
+        //   document.getElementById("headline").innerText = "Bonjour Chérie!";
+        //   document.getElementById("countdown").style.display = "none";
+        //   document.getElementById("canvas").style.display = "none";
+        //   document.body.style.backgroundColor = "#ffd54f"; 
+        //   document.getElementById("content").style.display = "block";
+        //   clearInterval(x);
+        // }
         //seconds
       }, 0)
   }());
